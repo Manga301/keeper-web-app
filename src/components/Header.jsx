85eef79dc1918696fb081ede47520627ab499de8
Mangaliso <mangaliso.twala@gmail.com>
2022-01-05 22:37:23 +0200

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import LocalActivityIcon from "@material-ui/icons/LocalActivity";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <LocalActivityIcon /> Keeper
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
